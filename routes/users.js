9a60eec7fa3c1556d187ed8989c6569b93ddcc6d
mfbernasol <mfbernasol@gmail.com>
2022-01-07 22:33:07 -0600

    create authentication rotue

--- routes/users.js
+++ routes/users.js
@@ -1,8 +1,8 @@
-const bcrypt = require('bcryptjs');
 const express = require('express');
-const jwt = require('jsonwebtoken');
-const config = require('config')
 const router = express.Router();
+const bcrypt = require('bcryptjs');
+const jwt = require('jsonwebtoken');
+const config = require('config');
 const { body, validationResult } = require('express-validator');
 
 const User = require('../models/User');
@@ -49,14 +49,19 @@ router.post(
       const payload = {
         user: {
           id: user.id,
-        }
+        },
       };
-      jwt.sign(payload,config.get('jwtSecret'),{
-          expiresIn: 360000
-      }, (err, token) => {
-          if(err) throw err;
-          res.json({token})
-      })
+      jwt.sign(
+        payload,
+        config.get('jwtSecret'),
+        {
+          expiresIn: 360000,
+        },
+        (err, token) => {
+          if (err) throw err;
+          res.json({ token });
+        }
+      );
     } catch (err) {
       console.error(err.message);
       res.status(500).send('Server Error');
