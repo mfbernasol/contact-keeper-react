441a0fa0305a942da6c07efd0edb24008a93cc67
mfbernasol <mfbernasol@gmail.com>
2022-01-12 20:14:27 -0600

    creating user registration form

--- client/src/components/auth/Register.js
+++ client/src/components/auth/Register.js
@@ -1,6 +1,21 @@
-import React, { useState } from 'react';
+import React, { useState, useContext, useEffect } from 'react';
+import AlertContext from '../../context/alert/alertContext';
+import AuthContext from '../../context/auth/authContext';
 
 const Register = () => {
+  const alertContext = useContext(AlertContext);
+  const authContext = useContext(AuthContext);
+
+  const { setAlert } = alertContext;
+  const { register, error, clearErrors } = authContext;
+
+  useEffect(() => {
+    if (error === 'User already exists') {
+      setAlert(error, 'danger');
+      clearErrors();
+    }
+  }, [error]);
+
   const [user, setUser] = useState({
     name: '',
     email: '',
@@ -14,52 +29,77 @@ const Register = () => {
 
   const onSubmit = (e) => {
     e.preventDefault();
-    console.log('Register submit');
+    if (name === '' || email === '' || password === '') {
+      setAlert('Please enter all fields', 'danger');
+    } else if (password !== password) {
+      setAlert('Passwords do not match', 'danger');
+    } else {
+      register({
+        name,
+        email,
+        password,
+      });
+    }
   };
 
   return (
     <div className='form-container'>
       <h1>
         Account <span className='text-primary'>Register</span>
-        <form onSubmit={onSubmit}>
-          <div className='form-group'>
-            <label htmlFor='name'>Name</label>
-            <input type='text' name='name' value={name} onChange={onChange} />
-          </div>
-          <div className='form-group'>
-            <label htmlFor='email'>Email Address</label>
-            <input
-              type='email'
-              name='email'
-              value={email}
-              onChange={onChange}
-            />
-          </div>
-          <div className='form-group'>
-            <label htmlFor='password'>Password</label>
-            <input
-              type='password'
-              name='password'
-              value={password}
-              onChange={onChange}
-            />
-          </div>
-          <div className='form-group'>
-            <label htmlFor='password2'>Confirm Password</label>
-            <input
-              type='password'
-              name='password2'
-              value={password2}
-              onChange={onChange}
-            />
-          </div>
-          <input
-            type='submit'
-            value='Register'
-            className='btn btn-primary btn-block'
-          />
-        </form>
       </h1>
+      <form onSubmit={onSubmit}>
+        <div className='form-group'>
+          <label htmlFor='name'>Name</label>
+          <input
+            id='name'
+            type='text'
+            name='name'
+            value={name}
+            onChange={onChange}
+            required
+          />
+        </div>
+        <div className='form-group'>
+          <label htmlFor='email'>Email Address</label>
+          <input
+            id='email'
+            type='email'
+            name='email'
+            value={email}
+            onChange={onChange}
+            required
+          />
+        </div>
+        <div className='form-group'>
+          <label htmlFor='password'>Password</label>
+          <input
+            id='password'
+            type='password'
+            name='password'
+            value={password}
+            onChange={onChange}
+            required
+            minLength='6'
+          />
+        </div>
+        <div className='form-group'>
+          <label htmlFor='password2'>Confirm Password</label>
+          <input
+            id='password2'
+            type='password'
+            name='password2'
+            value={password2}
+            onChange={onChange}
+            required
+            minLength='6'
+          />
+        </div>
+        <input
+          type='submit'
+          value='Register'
+          className='btn btn-primary btn-block'
+        />
+      </form>
     </div>
   );
 };
